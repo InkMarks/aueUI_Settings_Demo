d2da5b2d06997b481dd49e05ff97fedb06228c37
InkMarks <2438726882@qq.com>
2020-01-21 12:03:12 +0800

    1.0.0-B3

--- app/src/main/java/com/aueui/settings/Activities/About.java
+++ app/src/main/java/com/aueui/settings/Activities/About.java
@@ -1,16 +1,23 @@
 package com.aueui.settings.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
 import com.aueui.settings.Base.BaseActivity;
 import com.aueui.settings.R;
 
 public class About extends BaseActivity {
+
+    private Object view;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
 
     }
 }
