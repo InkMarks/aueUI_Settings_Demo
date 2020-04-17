d3d54bf4b346474d1ca21086462244653e952419
InkMarks <2438726882@qq.com>
2020-04-17 10:05:00 +0800

    beta

--- app/src/main/java/com/aueui/settings/Activities/About.java
+++ app/src/main/java/com/aueui/settings/Activities/About.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.aueui.settings.Base.BaseActivity;
+import com.aueui.settings.MainActivity;
 import com.aueui.settings.R;
 
 public class About extends BaseActivity {
@@ -17,7 +18,14 @@ public class About extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
-
+        Button btn_back = findViewById(R.id.about_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(About.this, MainActivity.class));
+            }
+        });
 
     }
 }
